b43a92bf1e07c57faeeb9a752fb50d9f22ab1a1b
Esperansa1 <6or2005@gmail.com>
2023-04-08 02:31:42 +0300

    cleaned up code

--- Algorithms/bfs.py
+++ Algorithms/bfs.py
@@ -1,38 +1,22 @@
 from algorithm import Algorithm
-import math
+
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-PINK = (255, 192, 203)
 
 
 class BFS(Algorithm):
     def __init__(self):
         super().__init__("BFS")
-        self.open_set = []
-        self.closed_set = []
-
-    def initialize(self):
-        super().initialize()
-        for row in self.grid:
-            for cell in row:
-                cell.parent = None
 
     def loop_algorithm(self):
-        if len(self.open_set) == 0:
-            self.run_simulation = False
+        if self.is_finished():
             return
 
         self.color_sets()
-        current_cell = self.open_set.pop(0)
 
+        current_cell = self.open_set.pop(0)
         self.closed_set.append(current_cell)
 
-        if current_cell == self.end_point:
-            self.reconstruct_path()
-            self.run_simulation = False
+        self.is_goal(current_cell)
 
         for neighbour in current_cell.neighbours:
             if neighbour.color == BLACK or neighbour in self.closed_set:
